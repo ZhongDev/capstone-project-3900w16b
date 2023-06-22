3db794c196ce6708e9875873480b05cdec23a811
dowzhong <dowzhong@gmail.com>
2023-06-22 21:46:16 +1000

    feat: login

---
+++ client/src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/LoginForm";
+import { Title } from "@mantine/core";
+
+export default function Login() {
+  return (
+    <>
+      <Title mb="xl" mt="xl" align="center" color="gold.5">
+        Plateholder
+      </Title>
+      <LoginForm />
+    </>
+  );
+}
