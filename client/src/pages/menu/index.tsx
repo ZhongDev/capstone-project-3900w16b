7fdf03ff43bcc11448244959622d2347e24d904f
Jason Zhong <47954501+ZhongDev@users.noreply.github.com>
2023-06-24 14:49:50 +1000

    feat: navbar

---
+++ client/src/pages/menu/index.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "@/components/Sidebar";
+import { AppShell, Navbar, Header } from "@mantine/core";
+
+export default function MenuManagement() {
+  return (
+    <Sidebar>
+      <b>hi</b>
+      <p>sd</p>
+    </Sidebar>
+  );
+}
