f715d8abdcd43181d78e52fbfa5c86d3a69cfcef
Jason Zhong <47954501+ZhongDev@users.noreply.github.com>
2023-07-07 11:25:21 +1000

    chore: formatting

--- server/schema/restaurant.ts
+++ server/schema/restaurant.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const CreateRestaurantRequest = z.object({
-    email: z.string(),
-    name: z.string(),
-    password: z.string(),
+  email: z.string(),
+  name: z.string(),
+  password: z.string(),
 });
 
 export const LoginRequest = z.object({
-    email: z.string(),
-    password: z.string(),
+  email: z.string(),
+  password: z.string(),
 });
