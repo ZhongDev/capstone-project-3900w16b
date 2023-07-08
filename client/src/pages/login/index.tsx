abaf09892f00ca1bb7f612f7ce2a61e4f5bf1c01
5309018 <ray.chen01011988@gmail.com>
2023-07-08 18:09:30 +1000

    style: change to LF

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -3,14 +3,14 @@ import { Title } from "@mantine/core";
 import Link from "next/link";
 
 export default function Login() {
-    return (
-        <>
-            <Link href="/">
-                <Title mb="xl" mt="xl" align="center" color="gold.5">
-                    Plateholder
-                </Title>
-            </Link>
-            <LoginForm />
-        </>
-    );
+  return (
+    <>
+      <Link href="/">
+        <Title mb="xl" mt="xl" align="center" color="gold.5">
+          Plateholder
+        </Title>
+      </Link>
+      <LoginForm />
+    </>
+  );
 }
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -3,14 +3,14 @@ import { Title } from "@mantine/core";
 import Link from "next/link";
 
 export default function Register() {
-    return (
-        <>
-            <Link href="/">
-                <Title mb="xl" mt="xl" align="center" color="gold.5">
-                    Plateholder
-                </Title>
-            </Link>
-            <RegisterForm />
-        </>
-    );
+  return (
+    <>
+      <Link href="/">
+        <Title mb="xl" mt="xl" align="center" color="gold.5">
+          Plateholder
+        </Title>
+      </Link>
+      <RegisterForm />
+    </>
+  );
 }
