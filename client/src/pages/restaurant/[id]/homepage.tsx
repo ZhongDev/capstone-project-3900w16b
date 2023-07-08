b7c3c780d304a1ca79da5c055a050a202fc9c10b
Darren <135718504+tofubofu007@users.noreply.github.com>
2023-07-08 17:55:10 +1000

    Create homepage.tsx

---
+++ client/src/pages/restaurant/[id]/homepage.tsx
@@ -0,0 +1,25 @@
+import {
+    Title,
+    Drawer,
+    createStyles,
+    Header,
+    Container,
+    Text,
+    Button,
+    Burger,
+    Group,
+    rem,
+    ScrollArea,
+    Divider,
+    UnstyledButton,
+    Center,
+    Box,
+    Collapse,
+} from "@mantine/core";
+import Image from "next/image";
+import { getMe } from "@/api/auth";
+import useSWR from "swr";
+import PlateHolderImg from "@/public/img/landing_ramen.png";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useDisclosure } from "@mantine/hooks";
