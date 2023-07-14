3b901baeb9b522cfef16efbdf2f55fc4a35da449
Darren <135718504+tofubofu007@users.noreply.github.com>
2023-07-14 11:39:49 +1000

    feat/DROP-38 making table input homepage
    making table input homepage for customer

--- client/src/pages/restaurant/[id]/homepage.tsx
+++ client/src/pages/restaurant/[id]/homepage.tsx
@@ -19,7 +19,75 @@ import {
 import Image from "next/image";
 import { getMe } from "@/api/auth";
 import useSWR from "swr";
-import PlateHolderImg from "@/public/img/landing_ramen.png";
+import PlateHolderImg from "@/public/img/pikachu_food.jpg";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useDisclosure } from "@mantine/hooks";
+import { wrap } from "module";
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    display: "flex",
+    alignItems: "center",
+  },
+  image: {
+    [theme.fn.smallerThan("xs")]: {
+      float: "right",
+    },
+  },
+  button: {
+    "&:hover": {
+      color: theme.colors.gray[0],
+      backgroundColor: theme.colors.gold[6],
+    },
+    [theme.fn.smallerThan("xs")]: {
+      display: "none",
+    },
+  },
+}));
+
+export default function HomePage() {
+  const { classes, theme } = useStyles();
+  const router = useRouter();
+
+  return (
+    <>
+      <div>
+        <Title mb="xl" mt="xl" size="3rem" color="gold.5" ta="center">
+          Plateholder
+        </Title>
+        <Text fw={500} size="2rem" ta="center">
+          Simply
+          <br></br>
+          Unbeatable Food
+        </Text>
+        <Group position="center">
+          <Button
+            className={classes.button}
+            variant="outline"
+            radius="xl"
+            size="lg"
+            onClick={() => router.push("/register")}
+          >
+            Order Now
+          </Button>
+          <Button
+            className={classes.button}
+            variant="outline"
+            radius="xl"
+            size="lg"
+            onClick={() => router.push("/login")}
+          >
+            Scan QR Code
+          </Button>
+        </Group>
+        <Image
+          className={classes.image}
+          src={PlateHolderImg}
+          sizes="(max-width: 800px) 100vw, (max-width: 582px) 50vw, 33vw"
+          alt="Picture of sample restaurant food"
+        ></Image>
+      </div>
+    </>
+  );
+}
