13d78ce4236729d8a186d5d314253287fc316e6c
Darren <135718504+tofubofu007@users.noreply.github.com>
2023-07-17 23:54:49 +1000

    changed user menu link
    changed user menu link from /restaurant/[restaurantId] to /menu/[restaurantId]

--- client/src/pages/restaurant/[id]/table_selection.tsx
+++ client/src/pages/restaurant/[id]/table_selection.tsx
@@ -66,7 +66,7 @@ export default function HomePage() {
     const restaurantId = Number(router.query.id);
     const name = newTableName;
     checkTable({ restaurantId, name })
-      .then(() => router.push(`../${router.query.id}`))
+      .then(() => router.push(`/menu/${router.query.id}`))
       .catch((err) => {
         setErrorMessage(err.msg);
       });
